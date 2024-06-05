a625f2987338d20a9f8456a535de9ac94b7d0fcc
Sumon-Mia <sumon.wdd@gmail.com>
2024-06-05 14:22:57 +0600

    ongoing

--- app/component/doctor.js
+++ app/component/doctor.js
@@ -1,142 +1,41 @@
+
+'use client'
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-const doctors = [
-  {
-    name: "Dr. Mariam Faruqui (Shati)",
-    specialty: "Gynecologist & Obstetrics",
-    degrees:
-      "MBBS, FCPS (ObGyn), MMed (UK), MSc (Embryology), PhD (Reproduction Medicine)",
-    position: "Professor (Gynecology & Obstetrics)",
-    institution: "Institute of Child and Mother Health (ICMH)",
-    logo: "/hospital-logo.png",
-  },
-  {
-     specialty: "Cardiac Surgeon",
-    name: "Dr. Lutfor Rahman",
-    degrees: "MBBS, DGO (DU), MCPS, MS (DU), FCPS",
-    experience: "20+ Years of Experience",
-    hospital: "LabAid Specialized Hospital",
-    logo: "/hospital-logo.png",
-  },
-  {
-     specialty: "Cardiac Surgeon",
-    name: "Dr. Lutfor Rahman",
-    degrees: "MBBS, DGO (DU), MCPS, MS (DU), FCPS",
-    experience: "20+ Years of Experience",
-    hospital: "LabAid Specialized Hospital",
-    logo: "/hospital-logo.png",
-  },
-  {
-     specialty: "Cardiac Surgeon",
-    name: "Dr. Lutfor Rahman",
-    degrees: "MBBS, DGO (DU), MCPS, MS (DU), FCPS",
-    experience: "20+ Years of Experience",
-    hospital: "LabAid Specialized Hospital",
-    logo: "/hospital-logo.png",
-  },
-  {
-     specialty: "Cardiac Surgeon",
-    name: "Dr. Lutfor Rahman",
-    degrees: "MBBS, DGO (DU), MCPS, MS (DU), FCPS",
-    experience: "20+ Years of Experience",
-    hospital: "LabAid Specialized Hospital",
-    logo: "/hospital-logo.png",
-  },
-  {
-     specialty: "Cardiac Surgeon",
-    name: "Dr. Lutfor Rahman",
-    degrees: "MBBS, DGO (DU), MCPS, MS (DU), FCPS",
-    experience: "20+ Years of Experience",
-    hospital: "LabAid Specialized Hospital",
-    logo: "/hospital-logo.png",
-  },
-  {
-     specialty: "Cardiac Surgeon",
-    name: "Dr. Lutfor Rahman",
-    degrees: "MBBS, DGO (DU), MCPS, MS (DU), FCPS",
-    experience: "20+ Years of Experience",
-    hospital: "LabAid Specialized Hospital",
-    logo: "/hospital-logo.png",
-  },
-  {
-     specialty: "Cardiac Surgeon",
-    name: "Dr. Lutfor Rahman",
-    degrees: "MBBS, DGO (DU), MCPS, MS (DU), FCPS",
-    experience: "20+ Years of Experience",
-    hospital: "LabAid Specialized Hospital",
-    logo: "/hospital-logo.png",
-  },
-  {
-     specialty: "Cardiac Surgeon",
-    name: "Dr. Lutfor Rahman",
-    degrees: "MBBS, DGO (DU), MCPS, MS (DU), FCPS",
-    experience: "20+ Years of Experience",
-    hospital: "LabAid Specialized Hospital",
-    logo: "/hospital-logo.png",
-  },
-  {
-    specialty: "Cardiac Surgeon",
-   name: "Dr. Lutfor Rahman",
-   degrees: "MBBS, DGO (DU), MCPS, MS (DU), FCPS",
-   experience: "20+ Years of Experience",
-   hospital: "LabAid Specialized Hospital",
-   logo: "/hospital-logo.png",
- },
- {
-  specialty: "Cardiac Surgeon",
- name: "Dr. Lutfor Rahman",
- degrees: "MBBS, DGO (DU), MCPS, MS (DU), FCPS",
- experience: "20+ Years of Experience",
- hospital: "LabAid Specialized Hospital",
- logo: "/hospital-logo.png",
-},
-{
-  specialty: "Cardiac Surgeon",
- name: "Dr. Lutfor Rahman",
- degrees: "MBBS, DGO (DU), MCPS, MS (DU), FCPS",
- experience: "20+ Years of Experience",
- hospital: "LabAid Specialized Hospital",
- logo: "/hospital-logo.png",
-},
-{
-  specialty: "Cardiac Surgeon",
- name: "Dr. Lutfor Rahman",
- degrees: "MBBS, DGO (DU), MCPS, MS (DU), FCPS",
- experience: "20+ Years of Experience",
- hospital: "LabAid Specialized Hospital",
- logo: "/hospital-logo.png",
-},
-];
+export default function Indexpage (products) {
+  if (!products || !Array.isArray(products)) {
+    return <div>No products available</div>;
+  }
 
-const IndexPage = () => (
+return(
   <div>
-    <div>
-      {doctors.map((doctor, index) => (
-        <div
-          key={index}
-          className="flex justify-between border p-4 rounded-lg shadow-sm bg-white my-4"
-        >
-          <div>
-          <p className="text-gray-600 text-xl font-semibold my-2">{doctor.specialty}</p>
-          <Link href='/doctors-details'><h3 className="text-xl font-semibold">{doctor.name}</h3></Link>
-            <p className="text-gray-600">{doctor.degrees}</p>
-            <p className="text-gray-600">{doctor.position}</p>
-            <p className="text-gray-600">{doctor.institution}</p>
-            <p className="text-gray-600">{doctor.experience}</p>
-            <p className="text-gray-600">{doctor.hospital}</p>
-          </div>
-          <div className="flex flex-col justify-between items-end">
-            <Image src={doctor.logo} alt="doctor logo" width={150} height={150} />
-            <button className="mt-2 py-2 text-green-600 rounded-lg">
-              Book Appointment
-            </button>
-          </div>
+    {products.map((product) => (
+      <div
+        key={product.id}
+        className="flex justify-between border p-4 rounded-lg shadow-sm bg-white my-4"
+      >
+        <div>
+        <p className="text-gray-600 text-xl font-semibold my-2">{product.specialty}</p>
+        <Link href='/doctors-details'><h3 className="text-xl font-semibold">{product.name}</h3></Link>
+          <p className="text-gray-600">{product.degrees}</p>
+          <p className="text-gray-600">{product.position}</p>
+          <p className="text-gray-600">{product.institution}</p>
+          <p className="text-gray-600">{product.experience}</p>
+          <p className="text-gray-600">{product.hospital}</p>
         </div>
-      ))}
-    </div>
+        <div className="flex flex-col justify-between items-end">
+          <Image src={product.logo} alt="doctor logo" width={150} height={150} />
+          <button className="mt-2 py-2 text-green-600 rounded-lg">
+            Book Apointment
+          </button>
+        </div>
+      </div>
+    ))}
   </div>
-);
 
-export default IndexPage;
+);
+}
+
+
--- app/navbar/degree.js
+++ app/navbar/degree.js
@@ -1,60 +1,109 @@
-'use client'
+"use client";
 import { useState } from "react";
 import { AiOutlineDown, AiOutlineUp } from "react-icons/ai";
+import Products from "../component/doctorsData";
+import IndexPage from "../component/doctor";
 
-export default function Degree () {
-    const [show, setShow] = useState(true);
-    const [inputChecked,setInputChecked] = useState(false);
-    
-     function handleShowClick () {
-        setShow(!show)
-     }
-    return(
-        <div className="flex flex-col p-4 shadow-lg rounded-lg bg-white">
-          <div className="flex justify-between">
-          <div className="mb-2">
-            <p className="text-xl font-semibold">
-                Degree
-            </p>
-          </div>
-            <div>
-               <button onClick={handleShowClick}>
-               {show ? <AiOutlineDown size={20}/> : <AiOutlineUp size={20}/>}
-               </button>
-            </div>
-          </div>
-           {show && 
-           <div className="flex flex-col">
-           <label htmlFor="check-md" className="text-xl font-semibold mb-2 flex gap-2">
-                <input id="check-md" type="checkbox" className="w-4" />
-                <p>
-               Md
-                </p>
-            </label>
-            <label htmlFor="check-ms" className="text-xl font-semibold mb-2 flex gap-2">
-                <input 
-                id="check-ms" 
-                value="checked"
-                onClick={() => setInputChecked(e.target.checked)}
-                type="checkbox" 
-                className="w-4"
-                 />
-               MS
-            </label>
-            <label htmlFor="check-fcps" className="text-xl font-semibold mb-2 flex gap-2">
-                <input id="check-fcps" type="checkbox" className="w-4" />
-                FCPS
-            </label>
-            <label htmlFor="check-m.phil" className="text-xl font-semibold mb-2 flex gap-2">
-                <input id="check-m.phil" type="checkbox" className="w-4" />
-                M.Phil
-            </label>
-            <label htmlFor="check-diploma" className="text-xl font-semibold mb-2 flex gap-2">
-                <input id="check-diploma" type="checkbox" className="w-4"/>
-                Diploma
-            </label>
-           </div>
-           }
+export default function Degree() {
+  const [show, setShow] = useState(true);
+  const [selectedDegrees, setSelectedDegrees] = useState([]);
+
+  const handleCheckboxChange = (degree) => {
+    setSelectedDegrees((prevSelectedDegrees) =>
+      prevSelectedDegrees.includes(degree)
+        ? prevSelectedDegrees.filter((d) => d !== degree)
+        : [...prevSelectedDegrees, degree]
+    );
+  };
+  
+  const filteredProducts = selectedDegrees.length
+    ? Products.filter((Product) =>
+        selectedDegrees.some((degree) => Product.degrees.includes(degree))
+      )
+    : Products;
+
+  function handleShowClick() {
+    setShow(!show);
+  }
+  return (
+    <div className="flex flex-col p-4 shadow-lg rounded-lg bg-white">
+      <div className="flex justify-between">
+        <div className="mb-2">
+          <p className="text-xl font-semibold">Degree</p>
         </div>
-    )
+        <div>
+          <button onClick={handleShowClick}>
+            {show ? <AiOutlineDown size={20} /> : <AiOutlineUp size={20} />}
+          </button>
+        </div>
+      </div>
+      {show && (
+        <div className="flex flex-col">
+          <label
+            htmlFor="check-md"
+            className="text-xl font-semibold mb-2 flex gap-2"
+          >
+            <input
+              id="check-md"
+              type="checkbox"
+              value="Md"
+              onChange={() => handleCheckboxChange("MD")}
+              className="w-4"
+            />
+            Md
+          </label>
+          <label
+            htmlFor="check-ms"
+            className="text-xl font-semibold mb-2 flex gap-2"
+          >
+            <input
+              id="check-ms"
+              type="checkbox"
+              value="FCPS"
+              onChange={() => handleCheckboxChange("FCPS")}
+              className="w-4"
+            />
+            MS
+          </label>
+          <label
+            htmlFor="check-fcps"
+            className="text-xl font-semibold mb-2 flex gap-2"
+          >
+            <input id="check-fcps" type="checkbox" className="w-4" />
+            FCPS
+          </label>
+          <label
+            htmlFor="check-m.phil"
+            className="text-xl font-semibold mb-2 flex gap-2"
+          >
+            <input
+              id="check-m.phil"
+              type="checkbox"
+              value="M.Phil"
+              onChange={() => handleCheckboxChange("M.Phil")}
+              className="w-4"
+            />
+            M.Phil
+          </label>
+          <label
+            htmlFor="check-diploma"
+            className="text-xl font-semibold mb-2 flex gap-2"
+          >
+            <input
+              id="check-diploma"
+              type="checkbox"
+              value="Diploma"
+              onChange={() => handleCheckboxChange("Diploma")}
+              className="w-4"
+            />
+            Diploma
+          </label>
+        </div>
+      )}
+
+      <div>
+        <IndexPage products={filteredProducts}/>
+      </div>
+    </div>
+  );
 }
