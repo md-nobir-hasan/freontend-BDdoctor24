03e7a277088fded1610ebbb124c3931e1d9c1026
Md. Nobir Hasan <nobir.wd@gmail.com>
2024-06-08 15:19:52 +0600

    delete every thing

--- app/doctors-details/layout.js
+++
@@ -1,9 +0,0 @@
-export default function RootLayout({ children }) {
-    return (
-      <html lang="en">
-        <body className="bg-cyan-100">
-          {children}
-          </body>
-      </html>
-    );
-  }
